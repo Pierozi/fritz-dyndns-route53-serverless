1465cb663714de0a5133aeb71e806d577e322383
Pierre Tomasina <tomasinap@gmail.com>
2019-03-06 02:48:31 +0100

    lambda update route53

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ $(error You must specify bucket parameter)
 endif
 
 ## Package Cloud Formation template
-template-output.yml: src/template.yml
+package: src/template.yml src/app/main.js
 	aws --profile $(profile) --region $(region) \
 	cloudformation package \
 		--template-file src/template.yml \
@@ -16,16 +16,22 @@ template-output.yml: src/template.yml
 		--output-template-file template-output.yml
 
 ## Deploy Cloud Formation stack
-deploy: template-output.yml
+deploy: package
 	aws --profile $(profile) --region $(region) \
 		cloudformation deploy \
 		--template-file template-output.yml \
 		--capabilities CAPABILITY_NAMED_IAM \
 		--stack-name $(stack_name) \
-		--parameter-overrides DomainName=$(domainName) \
+		--parameter-overrides HostedZoneId=$(hostedZoneId) \
+			RsDomain=$(rsDomain) \
 			DynDnsUsername=$(dynDnsUsername) \
 			DynDnsPassword=$(dynDnsPassword)
 
 ## Sam Local API
 local:
-	sam local start-api --region $(region) -t src/template.yml
+	sam local start-api --region $(region) \
+		-t src/template.yml \
+		--parameter-overrides "ParameterKey=HostedZoneId,ParameterValue=$(hostedZoneId) \
+			ParameterKey=RsDomain,ParameterValue=$(rsDomain) \
+			ParameterKey=DynDnsUsername,ParameterValue=$(dynDnsUsername) \
+			ParameterKey=DynDnsPassword,ParameterValue=$(dynDnsPassword)"
--- src/app/main.js
+++ src/app/main.js
@@ -1,5 +1,7 @@
 'use strict';
-const aws = require('aws-sdk');
+const AWS = require('aws-sdk');
+const route53 = new AWS.Route53({apiVersion: '2013-04-01'});
+const { hostedZoneId, rsDomain, username, password } = process.env;
 
 /**
  * Format API Gateway response
@@ -16,7 +18,50 @@ const createResponse = (statusCode, body) => {
   };
 };
 
-exports.handler = async (event, context) => {
-  //return createResponse(403, {"error": "Not authorized"});
-  return createResponse(200, {"status": true});
+const updateRecord = (ip, callback) => {
+  const params = {
+    ChangeBatch: {
+      Changes: [{
+        Action: "UPSERT",
+        ResourceRecordSet: {
+          Name: rsDomain,
+          ResourceRecords: [{
+            Value: ip
+          }],
+          TTL: 60,
+          Type: "A"
+        }
+      }],
+      Comment: "FRITZ!Box record"
+    },
+    HostedZoneId: hostedZoneId
+  };
+  route53.changeResourceRecordSets(params, (err, data) => {
+    callback(err, data && data.hasOwnProperty('ChangeInfo') ? data.ChangeInfo.Status : 'FAIL');
+  });
+};
+
+const checkBasicAuth = (headers) => {
+  if (!headers.hasOwnProperty('Authorization')) {
+    return false;
+  }
+  const basic = new Buffer(headers.Authorization.split(' ')[1], 'base64').toString();
+  const reqCredentials = basic.split(':');
+
+  return !(reqCredentials[0] !== username || reqCredentials[1] !== password);
+};
+
+exports.handler = (event, context, callback) => {
+  if (false === checkBasicAuth(event.headers)) {
+    callback(null, createResponse(403, {"error": "Not authorized"}));
+  }
+  if (!event.headers.hasOwnProperty('X-Forwarded-For')) {
+    callback(null, createResponse(500, {"error": "missing ip in proxy http headers"}));
+  }
+  updateRecord(event.headers['X-Forwarded-For'], (err, success) => {
+    if (err) {
+      console.error(err);
+    }
+    callback(err, createResponse(200, {"status": success}));
+  });
 };
